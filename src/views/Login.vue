a9b243e4282e707314f208b4a2fdd9f72be58b5f
Aji Pratama <47959982+ajipratama11@users.noreply.github.com>
2022-01-04 14:48:01 +0700

    u

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -48,6 +48,7 @@ export default {
   },
   methods: {
     async handleSubmit() {
+      const password = this.password;
       let encryptor = new JSEncrypt(); // New JSEncrypt Object
 
       let publicKey = `MIGeMA0GCSqGSIb3DQEBAQUAA4GMADCBiAKBgFR3ZVqHiPr/KuHtMGRHtGbRR5Xl
@@ -56,13 +57,15 @@ HuoLJPbxdkqx/wHCc+F1EfQDLc2qofV1R/SuJVFx7GsSez+cqJYMykaZLCwFYetz
 k1+Q2tGlKXoUbwKlAgMBAAE=`; //Paste in previous generations, which can be passed in from the background during actual development
 
       encryptor.setPublicKey(publicKey); // Set Public Key
-
+      console.log(password)
       try {
         const response = await axios.post("login", {
           email: this.email,
-          password: encryptor.encrypt(this.password),
+          password: encryptor.encrypt(password),
         });
+        const pw = encryptor.encrypt(password)
         console.log(response);
+        console.log(pw);
         localStorage.setItem("token", response.data.token);
         this.$store.dispatch("user", response.data.user);
         this.$router.push("/");
